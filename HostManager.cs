4dbb0005c05504f0792175910b2a30b0b5463316
Don Kackman <dkackman@gmail.com>
2023-12-08 10:11:33 -0600

    CreateCommandWithSingleOption

--- HostManager.cs
+++ HostManager.cs
@@ -1,7 +1,6 @@
 namespace MirrorManager;
 
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
 
--- Program.cs
+++ Program.cs
@@ -3,61 +3,65 @@ using System.CommandLine.Parsing;
 
 namespace MirrorManager;
 
-class Program
+static class Program
 {
     static async Task<int> Main(string[] args)
     {
         var rootCommand = new RootCommand("Manages local chia data layer mirrors and subscriptions.");
 
-        var unsubscribeCommand = new Command("unsubscribe-all", "Unsubscribes from all stores.");
-        var retainOption = new Option<bool>(
-            new[] { "--retain", "-r" },
-            getDefaultValue: () => false,
-            description: "Whether to retain files when unsubscribing.");
-        unsubscribeCommand.AddOption(retainOption);
-        unsubscribeCommand.SetHandler(async (retain) =>
-        {
-            await StoreManager.UnsubscribeAll(retain);
-        }, retainOption);
+        var unsubscribeCommand = CreateCommandWithSingleOption(
+            "unsubscribe",
+            "Unsubscribe from all stores.",
+            "retain",
+            "Whether to retain files when unsubscribing.",
+            false,
+            retain => StoreManager.UnsubscribeAll(retain));
 
-        var unMirrorCommand = new Command("unmirror-all", "Unmirrors all stores.");
-        var feeOption = new Option<ulong>(
-            new[] { "--fee", "-f" },
-            getDefaultValue: () => 0,
-            description: "Fee to use for each removal transaction.");
-        unMirrorCommand.AddOption(feeOption);
-        unMirrorCommand.SetHandler(async (fee) =>
-        {
-            await StoreManager.UnmirrorAll(fee);
-        }, feeOption);
+        var unmirrorCommand = CreateCommandWithSingleOption(
+            "unmirror",
+            "Unmirror all stores.",
+            "fee",
+            "Fee to use for each removal transaction.",
+            0UL,
+            fee => StoreManager.UnmirrorAll(fee));
 
-        var listAllCommand = new Command("list-all", "List all stores and their mirrors.");
-        var oursOnlyOption = new Option<bool>(
-            new[] { "--ours", "-o" },
-            getDefaultValue: () => true,
-            description: "Whether to only list our mirrors.");
-        listAllCommand.AddOption(oursOnlyOption);
-        listAllCommand.SetHandler(async (ours) =>
-        {
-            await StoreManager.ListAll(ours);
-        }, oursOnlyOption);
+        var listAllCommand = CreateCommandWithSingleOption(
+            "list-all",
+            "List all stores and their mirrors.",
+            "ours",
+            "Whether to only list our mirrors.",
+            true,
+            ours => StoreManager.ListAll(ours));
 
-        var checkCommand = new Command("check", "Verify that a mirror is accessible.");
-        var hostOption = new Option<string>(
-            new[] { "--host", "-h" },
-            getDefaultValue: () => "",
-            description: "The host address to check. Omit to check the local mirror.");
-        checkCommand.AddOption(hostOption);
-        checkCommand.SetHandler(async (host) =>
-        {
-            await HostManager.CheckHost(host);
-        }, hostOption);
+        var checkCommand = CreateCommandWithSingleOption(
+            "check",
+            "Verify that a mirror host is accessible.",
+            "host",
+            "The host address to check. Omit to check the local mirror.",
+            string.Empty,
+            host => HostManager.CheckHost(host));
 
         rootCommand.AddCommand(checkCommand);
         rootCommand.AddCommand(listAllCommand);
-        rootCommand.AddCommand(unMirrorCommand);
+        rootCommand.AddCommand(unmirrorCommand);
         rootCommand.AddCommand(unsubscribeCommand);
 
         return await rootCommand.InvokeAsync(args);
+    }
+
+    private static Command CreateCommandWithSingleOption<TOption>(string commandName, string commandDescription, string optionName, string optionDescription, TOption optionDefault, Func<TOption, Task> handler)
+    {
+        var command = new Command(commandName, commandDescription);
+        var option = new Option<TOption>(
+            new[] { $"--{optionName}", $"-{optionName[0]}" },
+            getDefaultValue: () => optionDefault,
+            description: optionDescription);
+        command.AddOption(option);
+        command.SetHandler(async (optionValue) =>
+        {
+            await handler(optionValue);
+        }, option);
+
+        return command;
     }
 }
